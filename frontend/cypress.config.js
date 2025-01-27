8528d9802496aee1f7d8dca5936481656db84805
mbialeuk <125869836+mbialeuk@users.noreply.github.com>
2025-01-27 23:54:15 +0100

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: '../tests/test_e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000', 
   },
 });
