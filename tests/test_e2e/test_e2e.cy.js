a6bd420f779f646634b76e9afc84f303646119c0
mbialeuk <125869836+mbialeuk@users.noreply.github.com>
2025-01-28 00:15:16 +0100

    Update test_e2e.cy.js

--- tests/test_e2e/test_e2e.cy.js
+++ tests/test_e2e/test_e2e.cy.js
@@ -1,14 +1,12 @@
 describe('Sentiment Analysis E2E Test', () => {
     it('Should submit a pos text and receive a pos sentiment', () => {
-      cy.visit('http://localhost:3000'); 
-      cy.get('textarea').type('I love this product!'); 
-      cy.get('#analyze-button').click();
-
-      cy.get('body').then((body) => {
-        cy.log(body.text()); 
-      });
+        cy.visit('http://localhost:3000').then(() => {
+            cy.log('Frontend loaded successfully.');
+        });      
+        
+        cy.get('textarea').type('I love this product!'); 
+        cy.get('#analyze-button').click();
   
       cy.contains('Sentiment: Positive', { timeout: 10000 }).should('exist');
     });
   });
-  
